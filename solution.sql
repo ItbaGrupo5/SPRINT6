3667ea51fed1e1005a02f34c48a9473506af49ad
Geronimo <geronimogs99@gmail.com>
2022-07-31 21:19:24 -0300

    4ta_parte

--- solution.sql
+++ solution.sql
@@ -94,3 +94,34 @@ SELECT * FROM vista_1
 ORDER BY EDAD ASC
 
 /* Insertar 5 nuevos clientes de un JSON */
+
+INSERT INTO cliente (customer_name, customer_surname, branch_id, customer_DNI, dob) 
+SELECT json_extract(json_each.value, '$.customer_name'), json_extract(json_each.value, '$.customer_surname'), json_extract(json_each.value, '$.branch_id'), json_extract(json_each.value, '$.customer_DNI'), json_extract(json_each.value, '$.customer_dob')
+FROM json_each('[
+    {"customer_name": "Lois", "customer_surname": "Stout", "customer_DNI": 47730534,"branch_id": 80,"customer_dob": "1984-07-07"
+    },
+    {"customer_name": "Hall", "customer_surname": "Mcconnell", "customer_DNI": 52055464,
+    "branch_id": 45,"customer_dob": "1968-04-30"
+    },
+    {"customer_name": "Hilel", "customer_surname": "Mclean", "customer_DNI": 43625213,
+    "branch_id": 77,"customer_dob": "1993-03-28"
+    },
+    {"customer_name": "Jin", "customer_surname": "Cooley", "customer_DNI": 21207908,"branch_id": 96,"customer_dob": "1959-08-24"
+    },
+    {"customer_name": "Gabriel", "customer_surname": "Harmon", "customer_DNI": 57063950,
+    "branch_id": 27,"customer_dob": "1976-04-01"}]');
+
+UPDATE cliente SET branch_id = 10
+WHERE customer_id > 500
+
+DELETE FROM cliente
+WHERE customer_name = "Noelle"
+/* No esta en la base de datos el cliente Noel David por eso borre otro */
+
+SELECT c.customer_name, c.customer_surname, p.loan_type, p.loan_date, p.loan_total
+FROM cliente c, prestamo p
+WHERE c.customer_id = p.customer_id
+ORDER BY p.loan_total DESC LIMIT 
+
+
+/* TERCER PROBLEMATICA */
